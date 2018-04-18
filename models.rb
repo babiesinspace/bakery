4bd0889bf77045392f0679989bfb10952207792c
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-04-18 11:08:14 -0400

    added a bakery class which will expect an array each of cookies, cakes and muffins instances (else an empty array). Added private methods to organize the inventory into one large hash, with sub hashes containing the categories of baked goods, each containing their own subhashes of specific flavors of each.

--- models.rb
+++ models.rb
@@ -1,3 +1,5 @@
+#nice to have: seasons sold - so that they can be showcased on the site but with disabled ordering button?
+
 class Cake
   attr_reader :type, :description, :price
   def initialize(type, description, price="$8")
@@ -24,4 +26,53 @@ class Muffin
     @price = price
   end 
 end 
+
+class Bakery
+  attr_accessor :cookies, :muffins, :cakes, :inventory
+
+  def initialize(cookie_array=[], cake_array=[], muffin_array=[])
+    @cookies = {}
+    @muffins = {}
+    @cakes = {}
+    @inventory = {}
+  end
+
+  def add_new_muffin(muffin)
+    create_muffin_inventory([muffin])
+    @inventory["muffins"] = @muffins
+  end
+
+  def add_new_cake(cake)
+    create_muffin_inventory([cake])
+    @inventory["cakes"] = @cakes
+  end
+
+  def add_new_cookie(cookie)
+    create_muffin_inventory([cookie])
+    @inventory["cookies"] = @cookies
+  end
+
+  private
+
+  def create_muffin_inventory(array_of_muffins)
+    array_of_muffins.each { |muffin| @muffins[muffin.type] = muffin }
+  end 
+
+  def create_cakes_inventory(array_of_cakes)
+    array_of_cakes.each { |cake| @cakes[cake.type] = cake }
+  end 
+
+  def create_cookies_inventory(array_of_cookies)
+    array_of_cookies.each { |cookie| @cookies[cookie.type] = cookie }
+  end
+
+  def create_inventory
+    @inventory["muffins"] = @muffins
+    @inventory["cakes"] = @cakes
+    @inventory["cookies"] = @cookies
+  end  
+
+end
+
+
 
