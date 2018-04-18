4efe2b4bb65b5e53b9ebef6e3d0cdd807cecc3a9
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-04-18 18:05:33 -0400

    added more pics because I have a problem. Added a form and tested it (getting 202 code). Added a modal to inform the user their email was added (not really checking but whatevs) - then redirect to home. Added files for all routes with very simple items

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source "https://rubygems.org" do
   gem "shotgun"
   gem "sendgrid-ruby"
   gem 'json'
+  gem 'sinatra-flash'
 end
--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
 require './models.rb'
 require 'sendgrid-ruby'
+require 'sinatra/flash'
 
 include SendGrid
+
 
 set :bakery, @bakery
 
@@ -33,16 +35,17 @@ end
 post '/contact' do
   @email = params[:email]
   @opinion = params[:opinion]
+  @name = params[:name]
 
-  from = Email.new(email: @email)
-  to = Email.new(email: 'allie.g.cooper@gmail.com')
-  subject = 'Add me to the list'
-  content = Content.new(type: 'text/plain', value: @opinion)
+  from = Email.new(email: 'allie.g.cooper@gmail.com')
+  to = Email.new(email: @email)
+  subject = "#{@name} would like to be added to the list to the list"
+  content = Content.new(type: 'text/plain', value: "Name: #{@name} \nThoughts: #{@opinion}")
   mail = Mail.new(from, subject, to, content)
 
   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
   response = sg.client.mail._('send').post(request_body: mail.to_json)
-  
+  redirect '/'
   if response.status_code == 401
     error_hash = JSON.parse(response.body)
     @errors = error_hash["errors"]
